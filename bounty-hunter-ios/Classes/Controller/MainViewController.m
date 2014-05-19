620e09ae9ca65cae526f6722aa3e32348996f4f5
YuJianSirius <YuJianSirius@aol.com>
2014-05-19 22:54:18 +0800

    add login

--- bounty-hunter-ios/Classes/Controller/MainViewController.m
+++ bounty-hunter-ios/Classes/Controller/MainViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "MainViewController.h"
+#import "AFHTTPRequestOperationManager.h"
+#import "AFHTTPRequestOperation.h"
+
 
 @interface MainViewController ()
 
@@ -18,12 +21,23 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    /*
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    NSDictionary *parameters = @{ @"username" :
+                                  @"password" : @"19922020a"};
+    [manager POST:@"http://localhost:1337/do/login" parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSLog(@"JSON: %@", responseObject);
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"Error: %@", error);
+    }];
+    */
+    
+    NSString *URLString = [NSString stringWithFormat:@"http://jwc.wyu.edu.cn/student/logon.asp"];
+    NSDictionary *parameters = @{@"UserCode": @"11080734", @"UserPwd" :;
+    [[AFHTTPRequestSerializer serializer] requestWithMethod:@"POST" URLString:URLString parameters:parameters];
+    
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
