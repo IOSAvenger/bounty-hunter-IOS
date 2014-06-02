2664d12e4dd90770489554d522235c22b49dbedb
YuJianSirius <YuJianSirius@aol.com>
2014-06-02 20:17:42 +0800

    updata TaskTableView

---
+++ bounty-hunter-ios/Classes/Controller/TaskDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  TaskDetailsViewController.h
+//  bounty-hunter-ios
+//
+//  Created by xiaojia on 6/2/14.
+//  Copyright (c) 2014 IOSAvenger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TaskDetailsViewController : UIViewController
+
+@end
